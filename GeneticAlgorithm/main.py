1e9c9cbb13fd98b5fad440cb01f78209f7c111a4
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-03-31 21:52:41 +0500

    Update main.py

--- GeneticAlgorithm/main.py
+++ GeneticAlgorithm/main.py
@@ -1,1 +1,13 @@
+from random import choices
+from typing import List
 
+genome = List[int]
+population = List[genome]
+
+def generate_genome(length):
+    return choices([0, 1], k=length)
+
+def generate_genome(size, genome_length):
+    return [generate_genome(genome_length) for _ in range(size)]
+
+def fitness(genome, things, weight_limit):
